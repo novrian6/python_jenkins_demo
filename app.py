091b4b77a164ffc203cacf5d10dcfa71e283c24a
Nova Novriansyah <liebera6@gmail.com>
2024-08-15 21:37:10 +0700

    merge feature a

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World-feature A!"
+    return "Hello, World-feature AAAA!"
 
 if __name__ == '__main__':
-    app.run(port=8083)
+    # Get the port from the environment variable, default to 8083 if not set
+    port = int(os.environ.get('FLASK_PORT', 8083))
+    app.run(port=port)
 
